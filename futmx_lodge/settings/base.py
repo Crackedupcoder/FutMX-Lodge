de6c0184be7f6da0a03e606ad1debef5c79b20d7
Abdul Samad <ibrahimabduldamad911@gmail.com>
2024-01-05 17:36:22 +0100

    setup the production environment

--- futmx_lodge/settings/base.py
+++ futmx_lodge/settings/base.py
@@ -12,16 +12,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cpo+x8*4cwkfwb2+d-1&)et!tg*#vt%yb&1+q7zn$_8_e&2umd'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
---
+++ futmx_lodge/settings/prod.py
@@ -0,0 +1,28 @@
+from .base import *
+import dj_database_url
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['*']
+
+ADMINS = [
+    ('Ibrahim Abdulsamad', 'ibrahimabduldamad911@gmail.com'),
+]
+
+DATABASES = {
+'default': {
+}
+}
+
+database_url = env('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
+
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_SSL_REDIRECT = True 
+SECURE_HSTS_SECONDS = 3600 # new
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True # new
+SECURE_HSTS_PRELOAD = True # new
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SESSION_COOKIE_SECURE = True # new
+CSRF_COOKIE_SECURE = True
